477f58764a6232a01d9e5c7e06651146bdaf0c35
bandiasek <ondrejbrendza@gmail.com>
2020-12-07 19:47:59 +0100

    Because of the changing of a name, we had to transfer already existing work into new one

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+require('firebase/auth')
+
+const firebaseConfig = {
+      apiKey: "AIzaSyBXwn60pDYke1ziJsQpsjNGJ7coEZP8q4k",
+      authDomain: "canza-e0d8e.firebaseapp.com",
+      projectId: "canza-e0d8e",
+      storageBucket: "canza-e0d8e.appspot.com",
+      messagingSenderId: "1047154785488",
+      appId: "1:1047154785488:web:b1c0b0cf66c4338b1cb2d5",
+      measurementId: "G-731RJRVQVW"
+    };
+
+    const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+    export const db = firebaseApp.firestore();
+
+    export const auth = firebase.auth();
