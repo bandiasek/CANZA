9fff8cf10d9b67b5d1023847bf45846f5f4c0b30
bandiasek <ondrejbrendza@gmail.com>
2020-12-11 15:22:46 +0100

    Slideshow + Text are responsive

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,18 +1,32 @@
 import React from 'react'
 
 import Slideshow from '../components/Slideshow'
+import Dash from '../assets/Dash.svg'
 
 import '../styles/reset.css'
 import '../styles/screens/Home.css'
 
 const Home = () => {
-            return (
-                  <div className="home__continerFluid">
-                      <div className="home__container">
-                        <Slideshow />
-                      </div>
-                  </div>
-            )
+	return (
+		<div className='home__continerFluid'>
+			<div className='home__container'>
+				<Slideshow className='home__slideShow' />
+				<div className='home__slideShowText'>
+					<h1>Pracuj na svojom tele</h1>
+					<h3>
+						<span className='home__slideShowTextEdit'>
+							<img src={Dash} alt='Dash' />
+						</span>
+						JE TVOJIM DOMOVOM PO CELÝ TVOJ ŽIVOT
+					</h3>
+				</div>
+
+        
+
+
+			</div>
+		</div>
+	)
 }
 
 export default Home
