9e1482b1e2baf42af15ed8603a8b374bb2a39aff
bandiasek <ondrejbrendza@gmail.com>
2021-01-28 18:33:43 +0100

    Welcome section done and responsive

--- src/screens/Food.js
+++ src/screens/Food.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
+import WelcomeImg from '../assets/food/WelcomeImg.svg'
+import BackImg from '../assets/food/BackImg.svg'
+
+import '../styles/screens/Food.css'
+
 const Food = () => {
             return (
-                  <div>
-                      <h1>Food</h1>  
+                  <div className="food__containerFluid">
+                      <div className="food__welcomeSection">
+                          <div className="food__welcomeSectionImg">
+                              <div className="food__welcomeSectionText">
+                                <h1>Papanie,  radosť </h1>
+                                
+                                <div className="food__welcomeSectionTextH2">
+                                    <div className="food__welcomeSectionDash" ></div>
+                                    <h2>V TÝCHTO DVOCH SLOVÁCH NEEXISTUJE ROZDIEL</h2>
+
+                                </div>
+                              </div>
+                              <img src={BackImg} alt="BackImg left bottom" />
+                          </div>
+                      </div>
+
+
                   </div>
             )
 }
