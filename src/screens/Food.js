cc4861371813966497792417c1d479364bb9017e
bandiasek <ondrejbrendza@gmail.com>
2021-01-30 16:48:34 +0100

    Introduction section is done and fully responsive

--- src/screens/Food.js
+++ src/screens/Food.js
@@ -6,6 +6,7 @@ import ItemLeafs from '../assets/food/ItemLeafs.svg'
 import ItemCart from '../assets/food/ItemCart.svg'
 import ItemApple from '../assets/food/ItemApple.svg'
 import ItemMeat from '../assets/food/ItemMeat.svg'
+import IntroductionImg from '../assets/food/Introduction.svg'
 
 import '../styles/screens/Food.css'
 
@@ -65,6 +66,16 @@ const Food = () => {
                                 <p>Pellentesque id ipsum vel ex malesuada porta. Nam at lectus in turpis condimentum porttitor et quis felis. Curabitur quis accumsan neque. </p>
                             </div>
                             
+                        </div>
+                    </div>
+
+                    <div className="food__introduction">
+                        <div className="food__introductionContianer">
+                            <img src={IntroductionImg} alt="Introduction Img floating left"/>
+                            <div className="food__introductionDescription">
+                                <h1>Vážne je dôležitá ?</h1>
+                                <p>Áno. Poďme si vysvetli prečo. Strava ako táka tvorí 80% úspechu bez ohľadu nato, aký je váš cieľ. Či chcete schudnúť, naberať, udržiavať si váhu alebo zlepšiť zdravie, bez kvalitnej stravy, stavebného materiálu pre telo, to nepôjde. Predstavte si vaše telo ako motor. Ak chcete aby fungoval správne a spoľahlivo musíte sa o neho starať a zásobovať ho kvalitným palivom (stravou). Môžete mať akýkoľvek silný motor, no pri nekvalitnom palive stráca na sile, kazí sa a nefunguje správne. </p>
+                            </div>
                         </div>
                     </div>
 
